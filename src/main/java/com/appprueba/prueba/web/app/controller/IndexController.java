6769355d7d9876e9ffc69679b86afec1ce1b3384
cosmologysk8 <a1999afg@gmail.com>
2022-10-10 11:14:24 +0200

    Proyecto para aprender Spring Boot con el curso de Udemy

---
+++ src/main/java/com/appprueba/prueba/web/app/controller/IndexController.java
@@ -0,0 +1,40 @@
+package com.appprueba.prueba.web.app.controller;
+import com.appprueba.prueba.web.app.models.Usuario;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/index")
+public class IndexController {
+
+    @GetMapping("/inicio")
+    public String inicio(Model model){
+        model.addAttribute("titulo", "Hola Mundo");
+        return "index";
+    }
+
+    @RequestMapping("/usuario")
+    public String perfil(Model model){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Adrian");
+        usuario.setApellidos("de la Fuente");
+        usuario.setEmail("a1999afg@gmail.com");
+        model.addAttribute("usuario",usuario);
+        model.addAttribute("titulo", "Perfil del usuari:".concat(usuario.getNombre()));
+        return "perfil";
+    }
+
+    @RequestMapping("/listar")
+    public String listar(Model model){
+        List<Usuario> usuarios = new ArrayList<>();
+        model.addAttribute("usuarios", usuarios);
+
+        return "listar";
+    }
+
+}
---
+++ src/main/java/com/appprueba/prueba/web/app/models/Usuario.java
@@ -0,0 +1,13 @@
+package com.appprueba.prueba.web.app.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Usuario {
+
+    private String nombre;
+    private String apellidos;
+    private String email;
+
+}
